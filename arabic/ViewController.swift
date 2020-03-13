0d2f48954d8d78946b42be8cfe1fc4e95141acd6
Bethany White <bethanyw@thedeltaschool.org>
2020-03-13 13:03:02 -0500

    Init

---
+++ arabic/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  arabic
+//
+//  Created by The Delta School Student on 1/27/20.
+//  Copyright © 2020 The Delta School Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func startButton(_ sender: Any) {
+        
+    }
+    @IBAction func travelButton(_ sender: Any) {
+        
+    }
+    @IBAction func numbersButton(_ sender: Any) {
+        
+    }
+    @IBAction func greetingsButton(_ sender: Any) {
+        
+    }
+    @IBAction func occupationsButton(_ sender: Any) {
+        
+    }
+    @IBAction func timeButton(_ sender: Any) {
+        
+    }
+    @IBAction func wordsPhrasesButton(_ sender: Any) {
+        
+    }
+    @IBAction func familyButton(_ sender: Any) {
+        
+    }
+    @IBAction func foodButton(_ sender: Any) {
+        
+    }
+    
+}
+
